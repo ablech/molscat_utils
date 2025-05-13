f425cd50eafdee79fb7a4ad74394534e26303a45
Alexander Blech <alexander.blech@fu-berlin.de>
2025-05-13 14:45:53 +0200

    Add click-based CLI

---
+++ src/pymolscat/cli.py
@@ -0,0 +1,39 @@
+import click
+import os
+from pathlib import Path
+from .wrapper import MolscatWrapper
+#from .io     import write_cross_sections, write_s_matrices
+
+@click.command()
+@click.argument('system', type=str)
+@click.argument('runfolder',
+                type=click.Path(
+                    exists=True, file_okay=False, dir_okay=True,
+                    writable=True, readable=True,
+                    ),
+                )
+@click.option('--molscat-bin', envvar='MOLSCAT_BIN', type=click.Path(exists=True), default=None,
+              help="Path to the Molscat executables. Defaults to the current directory or the MOLSCAT_BIN environment variable."
+              )
+def main(system, runfolder, molscat_bin):
+    """Run MOLSCAT.
+
+    \b
+    Arguments:
+        SYSTEM :
+            Name of the collision system. This is appended to the molscat executable name as f'molscat-{system}'
+        RUNFOLDER :
+            Path to the runfolder where the input files are located and output will be written.
+    """
+    runfolder = Path(runfolder)
+    if molscat_bin is None:
+        molscat_bin = Path.cwd() #if "MOLSCAT_BIN" not in os.environ else Path(os.environ["MOLSCAT_BIN"])
+    else:
+        molscat_bin = Path(molscat_bin)
+    executable = molscat_bin / f"molscat-{system}"
+    assert executable.exists(), f"Molscat executable {executable} does not exist"
+
+    wrapper = MolscatWrapper(executable)
+    wrapper.run(runfolder)
+
+    return 0
