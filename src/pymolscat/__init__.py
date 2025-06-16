e0be73d7cf3e96898c4853b8e54d83951325acc7
Alexander Blech <alexander.blech@fu-berlin.de>
2025-06-16 23:43:46 +0200

    Make version a dynamic attribute

--- src/pymolscat/__init__.py
+++ src/pymolscat/__init__.py
@@ -4,3 +4,13 @@ from . import (
     parser,
     cli,
 )
+
+__version__ = "0.1.0.dev0"
+
+import git
+try:
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha[0:7]
+except:
+    sha = "unknown"
+__git_version__ = __version__ + f"+{sha}"
