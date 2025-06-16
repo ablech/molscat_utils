8cef266e6293a8a099cdb8d828a4023b3303d39d
Alexander Blech <alexander.blech@fu-berlin.de>
2025-06-16 20:18:48 +0200

    Add parser CLI

--- src/pymolscat/cli.py
+++ src/pymolscat/cli.py
@@ -1,7 +1,10 @@
 import click
 import os
+import json
+import pickle
 from pathlib import Path
 from .wrapper import MolscatWrapper
+from .parser import parse_output
 #from .io     import write_cross_sections, write_s_matrices
 
 @click.command()
@@ -15,10 +18,10 @@ from .wrapper import MolscatWrapper
 @click.option('--molscat-bin', envvar='MOLSCAT_BIN', type=click.Path(exists=True), default=None,
               help="Path to the Molscat executables. Defaults to the current directory or the MOLSCAT_BIN environment variable."
               )
-@click.option('--quiet', type=bool, default=False,
+@click.option('--quiet', is_flag=True,
               help="Don't print Molscat output to stdout."
               )
-def main(system, runfolder, molscat_bin, quiet):
+def wrapper(system, runfolder, molscat_bin, quiet):
     """Run MOLSCAT.
 
     \b
@@ -39,4 +42,39 @@ def main(system, runfolder, molscat_bin, quiet):
     wrapper = MolscatWrapper(executable, quiet=quiet)
     wrapper.run(runfolder)
 
+    return 0
+
+
+@click.command()
+@click.argument('file',
+                type=click.Path(
+                    exists=True, file_okay=True, dir_okay=False,
+                    writable=False, readable=True,
+                    ),
+                )
+# NOTE: json serialization currently not possible due to numpy arrays
+@click.option('--write-json', is_flag=True,
+              help="Save parsed output in JSON format instead of as pickled object."
+              )
+def parser(file, write_json):
+    """Parse MOLSCAT output file.
+
+    \b
+    Arguments:
+        FILE :
+            The MOLSCAT output file to parse.
+
+    The parsed output is stored as FILE.parsed.pkl or FILE.parsed.json in the
+    same directory as FILE.
+    """
+    file = Path(file).absolute()
+    with open(file, 'r') as f:
+        output = f.readlines()
+    MolscatResult, output_data = parse_output(output)
+    if write_json:
+        with open(f"{file}.parsed.json", 'w') as f:
+            json.dump(output_data, f, indent=4)
+    else:
+        with open(f"{file}.parsed.pkl", 'wb') as f:
+            pickle.dump(output_data, f)
     return 0
